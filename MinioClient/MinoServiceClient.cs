20a4e1e5829a214c715e4bc7491e7880ae3f62fc
Matt Jarvis <mattj23@gmail.com>
2021-07-14 12:52:44 -0400

    initial

---
+++ MinioClient/MinoServiceClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinioClient
+{
+    /// <summary>
+    /// A client for an external Minio service.  Wraps the raw MinioClient from their nuget library, adding some features
+    /// which I found myself continuously having to re-implement.
+    /// </summary>
+    public class MinoServiceClient
+    {
+        private string _host;
+        private string _accessKey;
+        private string _secretKey;
+
+        public MinoServiceClient(string connectionString)
+        {
+            var parts = connectionString.Split(';');
+            if (parts.Length < 4)
+            {
+                throw new ArgumentException("Minio connection string needs four semicolon-separated values");
+            }
+
+            _host = parts[0];
+            _accessKey = parts[1];
+            _secretKey = parts[2];
+            Bucket = parts[3];
+        }
+
+        public string Bucket { get; }
+
+        public Minio.MinioClient GetClient()
+        {
+            return new Minio.MinioClient(_host, _accessKey, _secretKey).WithSSL();
+        }
+
+        public string FileHash(string fileName)
+        {
+            using var sha = SHA1.Create();
+            using var stream = File.OpenRead(fileName);
+            return BitConverter.ToString(sha.ComputeHash(stream)).Replace("-", string.Empty).ToLower();
+        }
+
+        public string ByteHash(byte[] bytes)
+        {
+            using var sha = SHA1.Create();
+            return BitConverter.ToString(sha.ComputeHash(bytes)).Replace("-", "").ToLower();
+        }
+
+        public async Task<byte[]> LoadSmallFileContents(string objectName)
+        {
+            try
+            {
+                using var memStream = new MemoryStream();
+                var client = GetClient();
+                await client.GetObjectAsync(Bucket, objectName, s => s.CopyTo(memStream));
+
+                memStream.Seek(0, SeekOrigin.Begin);
+                return memStream.GetBuffer();
+            }
+            catch (Minio.Exceptions.ObjectNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        public Task WriteBytesToFile(string objectName, byte[] bytes)
+        {
+            using var stream = new MemoryStream(bytes);
+            stream.Seek(0, SeekOrigin.Begin);
+            return GetClient().PutObjectAsync(Bucket, objectName, stream, stream.Length);
+        }
+
+        public async Task<string> LoadSmallFileText(string objectName)
+        {
+            var bytes = await LoadSmallFileContents(objectName);
+            return bytes == null ? null : Encoding.UTF8.GetString(bytes);
+        }
+
+        public async Task DeleteObject(string objectName)
+        {
+            var client = GetClient();
+            await client.RemoveObjectAsync(Bucket, objectName);
+        }
+
+    }
